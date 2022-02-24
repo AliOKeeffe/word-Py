8ba744b266e5841280d580bac533c4d0bf3484ae
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-02-24 11:23:16 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+WORD = 'snake'
+USER_INPUT = 'trend'
+
+# https://www.codegrepper.com/code-examples/python/python+turn+char+list+to+dictionary
+def get_user_guess():
+    """
+    Get guess input from ther user via the terminal
+    which must be a string 5 characters long
+    """
+    # print("Please enter your guess")
+    # print("This should be 5 characters long and needs to be an actual word")
+
+    # guess_str = input('Enter your guess here:\n')
+    # my_dict = {}
+    # for index, value in enumerate(guess_str):
+        # my_dict[index] = value
+    word_dict = {index:value for index, value in enumerate(WORD)}
+    print(word_dict)
+    
+    user_input_dict = {index:value for index, value in enumerate(USER_INPUT)}
+    print(user_input_dict)
+
+
+get_user_guess()
