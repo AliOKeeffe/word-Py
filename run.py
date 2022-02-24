4b13d436d9765f4e14e2fcc415289c5e29a513dc
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-02-24 12:54:56 +0000

    Add if statements to check if user guess letters match the word

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
-WORD = 'snake'
-USER_INPUT = 'trend'
+WORD = 'snakp'
+word_dict = {index : value for index, value in enumerate(WORD)}
+print(word_dict)
+USER_INPUT = 'supep'
 
 # https://www.codegrepper.com/code-examples/python/python+turn+char+list+to+dictionary
 def get_user_guess():
     """
     Get guess input from ther user via the terminal
     which must be a string 5 characters long
+    Convert string into dictionary with index and character
     """
     # print("Please enter your guess")
     # print("This should be 5 characters long and needs to be an actual word")
@@ -14,11 +17,50 @@ def get_user_guess():
     # my_dict = {}
     # for index, value in enumerate(guess_str):
         # my_dict[index] = value
-    word_dict = {index:value for index, value in enumerate(WORD)}
-    print(word_dict)
-    
+
     user_input_dict = {index:value for index, value in enumerate(USER_INPUT)}
     print(user_input_dict)
 
+    return user_input_dict
 
-get_user_guess()
+
+
+
+def check_matching_letters(user_guess):
+    """
+    Compare user guess again word
+    """
+    # # if user_guess == word_dict:
+    #     # print('you win')
+    # for letr in user_guess.values():
+    #     if letr not in word_dict.values():
+    #         print(f"word doesn't contain {letr}")
+    # # This check if the letter is in the word and in the correct position (i.e green)
+    # for letr in user_guess.values():
+    #     if letr in word_dict.values():
+    #         print(letr)
+
+    # # This checks if the letter is in the word and in the correct position (i.e green)
+    # for letr in user_guess.items():
+    #     if letr in word_dict.items():
+    #         print(letr)
+ 
+    for ind,letr in user_guess.items():
+        if letr == word_dict[ind]:
+            print(f'Green {letr}')
+        elif letr in word_dict.values():
+            print(f'Orange {letr}')
+        else:
+            print(f'Grey {letr}')
+ 
+    # word_dict.values()
+
+    # word_dict.items()
+
+
+
+
+# def check_letter_in_word():
+
+user_guess = get_user_guess()
+check_matching_letters(user_guess)
