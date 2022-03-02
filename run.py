2909a14af464acdaae5984a1092f839d3fa81217
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-03-02 15:38:15 +0000

    Add input validation for user word guess

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
-# import colorama for adding colour
+# import colorama for colour coding letters
 import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
+# import random to get random word for game
 import random
 
 # WORD = 'cheas'
 # word_dict = {index : value for index, value in enumerate(WORD)}
 # print(word_dict)
-USER_INPUT = 'books'
+# USER_INPUT = 'books'
 
-
+# class Wordle:
+    
 def get_game_word():
     """
     Open txt file and get random word
@@ -22,28 +24,53 @@ def get_game_word():
     game_word = {index : value for index, value in enumerate(random_word)}
     print(game_word)
     return game_word
-    
 
 
 # https://www.codegrepper.com/code-examples/python/python+turn+char+list+to+dictionary
 def get_user_guess():
     """
-    Get guess input from ther user via the terminal
-    which must be a string 5 characters long
+    Get  word guess input from ther user via the terminal
+    which must be a string 5 characters long and an actual word in Oxford Dict
     Convert string into dictionary with index and character
     """
-    # print("Please enter your guess")
-    # print("This should be 5 characters long and needs to be an actual word")
+    while True:
+        print("Please enter your guess")
+        print("This should be 5 characters long and needs to be an actual word")
 
-    # guess_str = input('Enter your guess here:\n')
-    # my_dict = {}
-    # for index, value in enumerate(guess_str):
-        # my_dict[index] = value
+        guess_str = input('Enter your guess here:\n').lower()
+        your_guess = {}
+        for index, value in enumerate(guess_str):
+            your_guess[index] = value
 
-    user_input_dict = {index:value for index, value in enumerate(USER_INPUT)}
-    print(user_input_dict)
+        # user_input_dict = {index:value for index, value in enumerate(USER_INPUT)}
+        
 
-    return user_input_dict
+        if validate_user_guess(guess_str):
+            print('Guess is valid')
+            break
+
+    return your_guess
+
+
+def validate_user_guess(guess):
+    """
+    Checks that user guess is valid - 5 letters and an actual word
+    """
+    try:
+        if len(guess) != 5:
+            raise ValueError(
+                f'That is not a 5 letter word. \n'
+            )
+        elif guess.isalpha() == False:
+            raise ValueError(
+                f'Please only enter letters not numbers \n'
+            )
+        # need to add in check to see if its an actual word
+    except ValueError as e:
+        print(f'Invalid data: {e}Please try again. \n')
+        return False
+    
+    return True
 
 
 
