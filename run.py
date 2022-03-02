92800ed4145b1e7dc909d90e7cd355a09cfee459
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-03-02 11:47:59 +0000

    Add text file with words and write code to get random word from it

--- run.py
+++ run.py
@@ -3,10 +3,27 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-WORD = 'cheas'
-word_dict = {index : value for index, value in enumerate(WORD)}
-print(word_dict)
-USER_INPUT = 'shaps'
+import random
+
+# WORD = 'cheas'
+# word_dict = {index : value for index, value in enumerate(WORD)}
+# print(word_dict)
+USER_INPUT = 'books'
+
+
+def get_game_word():
+    """
+    Open txt file and get random word
+    """
+    f = open('words.txt', 'r')
+    lines = f.read().splitlines()
+    f.close
+    random_word = random.choice(lines)
+    game_word = {index : value for index, value in enumerate(random_word)}
+    print(game_word)
+    return game_word
+    
+
 
 # https://www.codegrepper.com/code-examples/python/python+turn+char+list+to+dictionary
 def get_user_guess():
@@ -31,41 +48,42 @@ def get_user_guess():
 
 
 
-def check_matching_letters(user_guess):
+def check_matching_letters(game_word, user_guess):
     """
-    Compare user guess again word
+    Compare user guess against word
     """
 
     response_string = ""
 
-    if user_guess == word_dict:
+    if user_guess == game_word:
         print('Congratulations you have won!')
 
-
     else:
-        for i in range(0,5):
-            if user_guess[i] == word_dict[i]:
-                response_string += (Back.GREEN + user_guess[i])
-                word_dict[i] = "-"
-
-            elif user_guess[i] in word_dict.values():
-                response_string += (Back.YELLOW + user_guess[i])
-            
+        for ind,letr in user_guess.items():
+            if letr == game_word[ind]:
+                response_string += (Back.GREEN + letr)
+                
+            elif letr in game_word.values():
+                response_string += (Back.YELLOW + letr)
+                # need to ensure duplicate letter doesn't go orange if its already green
             else:
-                response_string += (Back.RED + user_guess[i])
+                response_string += (Back.RED + letr)
 
-    # for ind,letr in user_guess.items():
-    #     if letr == word_dict[ind]:
-    #         responseString += (Back.GREEN + letr)
+        # for i in range(0,5):
+        #     if user_guess[i] == word_dict[i]:
+        #         response_string += (Back.GREEN + user_guess[i])
+        #         word_dict[i] = "-"
+
+        #     elif user_guess[i] in word_dict.values():
+        #         response_string += (Back.YELLOW + user_guess[i])
             
-    #     elif letr in word_dict.values():
-    #         responseString += (Back.YELLOW + letr)
-    #         # need to ensure duplicate letter doesn't go orange if its already green
-    #     else:
-    #         responseString += (Back.RED + letr)
+        #     else:
+        #         response_string += (Back.RED + user_guess[i])
 
     print(response_string)
 # def check_letter_in_word():
 
+
+game_word = get_game_word()
 user_guess = get_user_guess()
-check_matching_letters(user_guess)
+check_matching_letters(game_word, user_guess)
